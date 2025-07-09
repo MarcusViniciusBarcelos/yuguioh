58eb9483f906aee7cbf5d3e189948ab91b492595
Barcelos <marcusbarcelos@rede.ulbra.br>
2025-07-09 15:01:08 -0300

    firebase

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -21,6 +21,9 @@ plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     // Android Gradle Plugin
     id("com.android.application")     version "8.10.1" apply false
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services") version("4.3.15") apply false
+    // END: FlutterFire Configuration
     // Kotlin Android Plugin
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
